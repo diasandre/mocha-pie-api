aea179037d49c25000f1ddf079fbc73468572025
andré <andredds12@gmail.com>
2021-05-20 21:22:44 -0300

    fix erros, add more validations

--- src/main/kotlin/com/github/diasandre/mochapie/util/Extensions.kt
+++ src/main/kotlin/com/github/diasandre/mochapie/util/Extensions.kt
@@ -1,13 +1,21 @@
 package com.github.diasandre.mochapie.util
 
 import com.github.diasandre.mochapie.enums.Header.STATUS
+import com.github.diasandre.mochapie.exceptions.ValidatorException
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import org.springframework.http.HttpStatus.OK
+import java.util.UUID
 import javax.servlet.http.HttpServletResponse
 
 fun String.removeWhitespaceAndSplit() = replace("\\s".toRegex(), "").split(",")
 
-fun Map<String, String>.getStatus() = this[STATUS.value]?.toInt()?.let(HttpStatus::valueOf) ?: HttpStatus.OK
+fun Map<String, String>.getStatus() = this[STATUS.value]?.toInt()?.let(HttpStatus::valueOf) ?: OK
 
 fun HttpServletResponse.setResponse(status: HttpStatus) {
     this.status = status.value()
 }
+
+fun String.toUUID() = runCatching {
+    UUID.fromString(this)
+}.getOrNull() ?: throw ValidatorException(INTERNAL_SERVER_ERROR, "Invalid UUID")
